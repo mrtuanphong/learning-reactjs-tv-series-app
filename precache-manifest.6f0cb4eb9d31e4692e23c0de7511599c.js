67225503a332d4a9c6e61fd343220f6b2552eea0
Phong Do <mrtuanphong@gmail.com>
2021-08-01 10:28:22 +0700

    Updates

---
+++ precache-manifest.6f0cb4eb9d31e4692e23c0de7511599c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b73ed527ac22dab42f7ccc40e36307e",
+    "url": "/tv-series-app/index.html"
+  },
+  {
+    "revision": "23fd6ceb6095dd8e31ec",
+    "url": "/tv-series-app/static/css/main.32077bc9.chunk.css"
+  },
+  {
+    "revision": "21e1ec9068fda9d9e6d3",
+    "url": "/tv-series-app/static/js/2.8f491ce2.chunk.js"
+  },
+  {
+    "revision": "23fd6ceb6095dd8e31ec",
+    "url": "/tv-series-app/static/js/main.c55fc4d8.chunk.js"
+  },
+  {
+    "revision": "7b445091c23959f2f510",
+    "url": "/tv-series-app/static/js/runtime-main.222616e6.js"
+  }
+]);
